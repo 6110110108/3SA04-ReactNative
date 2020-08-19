c94b0799cac05f0380654a346bac6fd9e64ba78a
SEOULCHA RATMUMAD <6110110108@psu.ac.th>
2020-08-20 04:27:37 +0700

    section CHALLENGE

--- component/Forecast.js
+++ component/Forecast.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet } from 'react-native'
 
 export default function Forecast(props) {
     return (
         <View>
-            <Text>{props.main}</Text>
-            <Text>{props.description}</Text>
-            <Text>{props.temp}</Text>
-            <Text>°C</Text>
+            <Text style={styles.mainText}>{props.main}</Text>
+            <Text style={styles.descriptionText}>{props.description}</Text>
+            <View style={{
+                flexDirection: 'row',
+                padding: 30,
+                }}>
+                <Text style={{paddingRight: 160}}></Text>
+                <Text style={styles.tempText}>{props.temp}</Text>
+                <Text style={styles.celsiusText}> °C</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create ({
+    mainText: {
+        fontSize: 30,
+        color: 'white',
+        paddingTop: 25,
+        textAlign: 'center',
+    },
+    descriptionText: {
+        fontSize: 20,
+        color: 'white',
+        paddingTop: 30,
+        textAlign: 'center',
+        
+    },
+    tempText: {
+        color: 'white',
+        fontSize: 30,
+    },
+    celsiusText: {
+        fontSize: 15,
+        textAlignVertical: 'center',
+        color: 'white',
+    },
+})
--- component/Weather.js
+++ component/Weather.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
-import { Text, ImageBackground, StyleSheet } from 'react-native'
+import { Text, ImageBackground, StyleSheet, View } from 'react-native'
 import Forecast from './Forecast'
 
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
-        main: '-',
-        description: '-',
+        main: 'main',
+        description: 'description',
         temp: 0
     })
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-            <Text>Zip code</Text>
-            <Text>{props.zipCode}</Text>
-            <Forecast {...forecastInfo}/>
+            <View style={styles.background}>
+                <Text style={styles.zipCodeText}>Zip code is {props.zipCode}</Text>
+                <Forecast {...forecastInfo}/>
+            </View>
         </ImageBackground>
     )
 }
@@ -24,5 +25,20 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: '100%',
         height: '100%'
+    },
+    zipCodeText: {
+        paddingTop: 20,
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center'
+    },
+    background: {
+        backgroundColor: 'black',
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        opacity: 0.65,
+        width: '100%',
+        height: '40%'
     }
 })
